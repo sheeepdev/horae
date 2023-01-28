4214a9cd31750a1abd217f444570925caa96f802
sheep <sheep@sheepdev.xyz>
2023-01-28 22:59:55 +0100

    init

---
+++ clock.js
@@ -0,0 +1,54 @@
+function showTime(){
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    var time = h + ":" + m + ":" + s;
+    document.getElementById("clock").innerText = time;
+    document.getElementById("clock").textContent = time;
+
+    setTimeout(showTime, 1000);
+}
+
+showTime();
+
+function date() {
+
+    var date = new Date();
+
+    var dow = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+      ],
+      month = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ],
+      day = date.getDate();
+  
+    var currentDate = dow[date.getDay()] + ", " + month[date.getMonth()] + " " + day;
+  
+    document.getElementsByClassName("date")[0].innerHTML = currentDate;
+}
+
+date();
